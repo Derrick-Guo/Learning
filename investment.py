d067493f6898937fccb96d57263459fe5cbdacb2
Derrick Guo <guo2624525@yahoo.co.jp>
2020-02-27 17:02:21 +0900

    Creation

---
+++ investment.py
@@ -0,0 +1,18 @@
+class Investment:
+	def __init__(self,balance):
+		self.balance=balance
+
+	def one(self,month=1):
+		for i in range(1,month+1):
+			shoushu=self.balance//3000*0.01
+			profit=shoushu*20000
+			self.balance+=profit
+		return self.balance
+
+	def two():
+		pass
+	
+
+my_investment=Investment(20000)
+print(my_investment.one(6))
+
