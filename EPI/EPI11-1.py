ed8a0a26b390d9d54fe75b1795180b5f128b428e
Derrick Guo <guo2624525@yahoo.co.jp>
2020-05-08 22:10:16 +0900

    upload

---
+++ EPI/EPI11-1.py
@@ -0,0 +1,17 @@
+def search_first_of_k(A,k):
+	l,r=0,len(A)-1
+	first_occurance=-1
+	while l<=r:
+		m=l+(r-l)//2
+		if A[m]>k:
+			r=m-1
+		elif A[m]<k:
+			l=m+1
+		else:
+			first_occurance=m
+			r=m-1
+	return first_occurance
+# Test
+a=[1,2,2,3,3,3]
+print(search_first_of_k(a,3))
+
