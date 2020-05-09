ebbec2fb67dbe896007cbf41bf66f45d9f9558d0
Derrick Guo <guo2624525@yahoo.co.jp>
2020-05-09 16:51:01 +0900

    update

--- EPI/EPI11-4.py
+++ EPI/EPI11-4.py
@@ -1,3 +1,4 @@
+# Tip: If the binary search doesn't find the exact number, the algorithm terminates and the square of l-1 is the closest number to k.  
 def square_root(k):
 	l,r=0,k
 	while l<=r:
@@ -9,5 +10,6 @@ def square_root(k):
 		else:
 			l=m+1
 	return l-1
+# Time complexity: O(logk), Space complexity: O(1)
 # Test
 print(square_root(16))
