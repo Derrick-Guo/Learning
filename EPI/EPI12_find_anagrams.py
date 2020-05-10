1666cbf6dcf2895ae7d475ac275bccd752e850df
Derrick Guo <guo2624525@yahoo.co.jp>
2020-05-10 21:35:56 +0900

    update

--- EPI/EPI12_find_anagrams.py
+++ EPI/EPI12_find_anagrams.py
@@ -8,6 +8,10 @@ def find_anagrams(strings):
 		sorted_string_to_anagrams[''.join(sorted(s))].append(s)
 	return [group for group in sorted_string_to_anagrams.values() if len(group)>=2]
 
+# Time complexity: The computation consists of n sorts and n insertions into the hash table. 
+# Sorting the keys has time complexity O(nmlogm), for m is the length of the longest string.
+# Insertions add a time complexity of O(nm), yielding O(nmlogm) time complexity in total.
+
 # Test
 strings=['elvis','lives','levis','money','silent','listen']
 print(find_anagrams(strings))
